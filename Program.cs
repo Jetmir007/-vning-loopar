3936c622db465293031d3dd7ceda266ab6c89fb7
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-09-04 12:09:56 +0200

    Övning loopa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*int tal = 1;
+while( tal <= 20)
+{
+    Console.WriteLine(tal);
+    tal++;
+}
+*/
+
+Console.Write("Skriv ett number");
+int tal = int.Parse(Console.ReadLine());
+while(tal <= 101)
+{
+    
+    Console.WriteLine(tal);
+    
+    tal++;
+
+
+}
