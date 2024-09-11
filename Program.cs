1ecc8a4678a0e28777c369dc830a23c37d200bb7
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-09-11 11:03:50 +0200

    For Loopar och medelvärde

--- Program.cs
+++ Program.cs
@@ -23,15 +23,28 @@ for(int i = int.Parse(Console.ReadLine());i<=101;i++)
 }
 */
 
+double biggest = 0;
+double smallest = 0;
 int antalTal;
 double summa = 0, input;
-Console.Write("Antal tal som ska matas in");
+Console.Write("Antal tal som ska matas in ");
 antalTal = Convert.ToInt32(Console.ReadLine());
 for (int i=1; i <= antalTal; i++)
 {
     Console.Write("Skriv värde " + i + ": ");
     input = Convert.ToDouble(Console.ReadLine());
     summa = summa + input;
+    if( i == 1){
+        biggest = input;
+        smallest = input;
+    }
+    if(smallest >= input){
+        smallest = input;
+    } else if (biggest <= input){
+        biggest = input;
+    }
 
 }
 Console.WriteLine("Medelvärdet är: " + (summa / antalTal));
+Console.WriteLine("Ditt största tal är: " + (biggest));
+Console.WriteLine("Ditt minsta tal är: " + (smallest));
